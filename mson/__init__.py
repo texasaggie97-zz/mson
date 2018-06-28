a5c7f5d16e596237ff6cb5b1b923b210982600eb
Mark Silva <github@markesilva.com>
2018-06-28 16:40:52 -0500

    Update parameters

--- mson/__init__.py
+++ mson/__init__.py
@@ -19,32 +19,34 @@ def detect_encoding(b):
 
 def dump(obj, fp, skipkeys=False, ensure_ascii=True, check_circular=True,
          allow_nan=True, cls=None, indent=None, separators=None,
-         encoding='utf-8', default=None, **kw):
+         default=None, sort_keys=False, **kw):
     return json.dump(
-        obj, fp, skipkeys, ensure_ascii, check_circular,
-        allow_nan, cls, indent, separators,
-        encoding, default, kw)
+        obj, fp, skipkeys=skipkeys, ensure_ascii=ensure_ascii, check_circular=check_circular,
+        allow_nan=allow_nan, cls=cls, indent=indent, separators=separators,
+        default=default, sort_keys=sort_keys, **kw)
 
 
 def dumps(obj, skipkeys=False, ensure_ascii=True, check_circular=True,
           allow_nan=True, cls=None, indent=None, separators=None,
-          encoding='utf-8', default=None, **kw):
+          default=None, sort_keys=False, **kw):
     return json.dumps(
-        obj, skipkeys, ensure_ascii, check_circular,
-        allow_nan, cls, indent, separators,
-        encoding, default, kw)
+        obj, skipkeys=skipkeys, ensure_ascii=ensure_ascii, check_circular=check_circular,
+        allow_nan=allow_nan, cls=cls, indent=indent, separators=separators,
+        default=default, sort_keys=sort_keys, **kw)
 
 
-def load(fp, encoding=None, cls=None, object_hook=None, parse_float=None,
+def load(fp, cls=None, object_hook=None, parse_float=None,
          parse_int=None, parse_constant=None, object_pairs_hook=None, **kw):
     s = fp.read()
     return loads(
-        s, encoding, cls, object_hook, parse_float,
-        parse_int, parse_constant, object_pairs_hook, kw)
+        s, cls=cls, object_hook=object_hook, parse_float=parse_float,
+        parse_int=parse_int, parse_constant=parse_constant, object_pairs_hook=object_pairs_hook, **kw)
 
 
-def loads(s, encoding=None, cls=None, object_hook=None, parse_float=None,
+def loads(s, cls=None, object_hook=None, parse_float=None,
           parse_int=None, parse_constant=None, object_pairs_hook=None, **kw):
     return json.loads(
-        s, encoding, cls, object_hook, parse_float,
-        parse_int, parse_constant, object_pairs_hook, kw)
+        s, cls=cls, object_hook=object_hook, parse_float=parse_float,
+        parse_int=parse_int, parse_constant=parse_constant, object_pairs_hook=object_pairs_hook, **kw)
+
+
